fac3e5ce0735482add973a2c50c6d00004d0ab22
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-09 19:51:48 +0000

    Add notice component

---
+++ src/components/Notice.js
@@ -0,0 +1,7 @@
+export default function Notice() {
+  return (
+    <div className="py-2 text-center text-xs text-white bg-blue-700">
+      <span className="mr-4 font-thin">Lorem ipsum dolor sit amet.</span><a href="/" className="underline">Take me there</a>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Notice from './components/Notice';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Notice />
   </React.StrictMode>,
   document.getElementById('root')
 );
