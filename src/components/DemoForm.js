1fea4f7cf844832311d188ba63ead359f7322b82
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-10 20:24:58 +0000

    Refactor DemoForm css

--- src/components/DemoForm.js
+++ src/components/DemoForm.js
@@ -2,13 +2,13 @@ export default function DemoForm() {
   return (
     <div className="max-w-3xl">
       <h1 className="mb-4 text-4xl lg:text-5xl xl:text-7xl font-semibold">Vel quis feugiat pharetra diam viverra quis.</h1>
-      <p className="mb-8 font-light lg:text-xl lg:font-light">Interdum enim platea sagittis vitae vitae. Odio adipiscing odio enim pharetra lorem ut sagittis nibh enim. Posuere.</p>
+      <p className="mb-8 lg:text-xl font-light">Interdum enim platea sagittis vitae vitae. Odio adipiscing odio enim pharetra lorem ut sagittis nibh enim. Posuere.</p>
       <form action="/" method="post" className="sm:flex">
-        <div className="mb-2 sm:mr-2 sm:">
+        <div className="mb-2 sm:mr-2">
           <input type="text" name="email" id="email" className="sm:px-4 w-full sm:w-80 h-12 text-center sm:text-left placeholder:text-black" placeholder="Enter your email address" required></input>
         </div>
         <div>
-          <input type="submit" value="Request a demo" className="btn w-full h-12 bg-blue"></input> 
+          <input type="submit" value="Request a demo" className="btn w-full bg-blue"></input> 
         </div>
       </form>
     </div>
