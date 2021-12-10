6f951eed570d361ddc1449402754b267b2c57c81
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-10 20:18:19 +0000

    Refactor Hero css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,12 +7,12 @@ import productChatLarge from '../images/product-chat-large.png';
 
 export default function Hero() {
   return (
-    <section className="flex flex-col items-center min-h-screen bg-midnight">
+    <section className="flex-center flex-col min-h-screen bg-midnight">
       <div className="w-full">
         <Notice />
         <Navbar />
       </div>
-      <div className="flex-1 flex justify-center items-center p-6 sm:p-24 w-full max-w-screen-2xl min-h-full">
+      <div className="flex-1 flex-center p-6 sm:p-24 w-full max-w-screen-2xl min-h-full">
         <div className="lg:grid grid-cols-2 gap-12">
           <div className="mb-8 text-white lg:mb-0">
             <DemoForm />
