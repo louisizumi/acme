3a53278504b725c69880f49adf7b542953f38f36
Louis Drinkwater <izumilouis95@gmail.com>
2021-12-11 11:26:37 +0000

    Add flex wrap to Notice

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -1,7 +1,7 @@
 export default function Notice() {
   return (
-    <div className="py-2 bg-blue text-center text-xs text-white">
-      <span className="mr-4 font-thin">Lorem ipsum dolor sit amet.</span><a href="/" className="underline">Take me there</a>
+    <div className="flex-center flex-wrap py-2 bg-blue text-center text-xs text-white">
+      <span className="mx-4 font-thin">Lorem ipsum dolor sit amet.</span><a href="/" className="mx-4 underline">Take me there</a>
     </div>
   )
 }
